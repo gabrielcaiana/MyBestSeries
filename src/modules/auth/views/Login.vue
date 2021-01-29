1307cac63ae0c2edef8534cf13f3d03789d70092
Gabriel <gabrielcaiana@hotmail.com>
2021-01-28 22:01:19 -0300

    estilizando página de login

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="login-page">
-		<form @submit.prevent="submit()">
-			<div class="card">
-				<div class="card-header">
-					Login
-				</div>
-				<div class="card-body">
-				<div class="form-group">
-					<input type="email" v-model="form.email" class="form-control" placeholder="E-mail" required>
+		<div class="overlay">
+			<form @submit.prevent="submit()">
+				<div class="card">
+					<div class="card-header">
+						Login
 					</div>
-							<div class="form-group">
-						<input type="password" v-model="form.password" class="form-control" placeholder="Senha" required>
-					</div>
+					<div class="card-body">
+					<div class="form-group">
+						<input type="email" v-model="form.email" class="form-control" placeholder="E-mail" required>
+						</div>
+								<div class="form-group">
+							<input type="password" v-model="form.password" class="form-control" placeholder="Senha" required>
+						</div>
 
-					<button class="btn btn-primary w-100">
-						Entrar
-					</button>
+						<button class="btn btn-primary w-100">
+							Entrar
+						</button>
+					</div>
 				</div>
-			</div>
-		</form>
+			</form>
+		</div>
   </div>
 </template>
 
@@ -45,6 +47,23 @@ export default {
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	background-image: url('/images/background.jpg');
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center center;
+	position: relative;
+
+	.overlay {
+		position: absolute;
+		background: rgba($color: #000000, $alpha: .6);
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		top: 0;
+		left: 0;
+	}
 
 	.card {
 		width: 400px;
